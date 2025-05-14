ae31f3ac7b95ac2bc0caa3c96c8e7788e2f8b5e7
Haresh <115217618+PoriyaBytez@users.noreply.github.com>
2025-05-14 17:34:19 +0530

    pdf design done

--- src/components/EncounterAndDialogue/EdList.js
+++ src/components/EncounterAndDialogue/EdList.js
@@ -127,7 +127,7 @@ const EdList = (props) => {
                     <Link
                       onClick={ClickHandler}
                       to={`/encounter-and-dialogue-single/${currentEd.id}`}
-                      state={{ pageNumber: currentPage, currentElement: currentEd.id }}
+                      state={{ pageNumber: currentPage }}
                       className="read-more"
                       style={{ fontSize: "15px" }}
                     >
