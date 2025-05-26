4e5fe2f4e20fe124364ce237a3b5c12b88523ae3
Haresh <115217618+PoriyaBytez@users.noreply.github.com>
2025-05-26 11:54:55 +0530

    changed the gallery design

--- src/components/pagetitle/PageTitle.js
+++ src/components/pagetitle/PageTitle.js
@@ -1,22 +1,15 @@
-import React from "react";
-
 const PageTitle = (props) => {
-  const height = props.pageTitle !== "" ? "200px" : "100px";
-  const marginBottom = props.pageTitle !== "" ? "1rem" : "4rem";
+  const isMobile = window.innerWidth <= 768;
+  const height = props.pageTitle !== "" ? "100px" : "0px";
+  const marginBottom = props.pageTitle !== "" ? "1rem" : "1rem";
   return (
-    <section
-      className="wpo-page-title"
-      style={{ minHeight: height, marginBottom }}
-    >
-      <div className="container">
+    <section className="wpo-page-title" style={{ minHeight: height, marginBottom }}>
+      <div className={`${isMobile ? "container" : "mt-5"}`}>
         <div className="row">
           <div className="col col-xs-12">
             <div className="wpo-breadcumb-wrap">
-              <h2 className="mt-5">{props.pageTitle}</h2>
-              <ol className="wpo-breadcumb-wrap">
-                {/* <li><Link to="/home">Home</Link></li> */}
-                {/* <li><span>{props.pagesub}</span></li> */}
-              </ol>
+              <h2 className={`${isMobile ? "m-0" : "mt-5"}`}>{props.pageTitle}</h2>
+              <ol className="wpo-breadcumb-wrap"></ol>
             </div>
           </div>
         </div>
